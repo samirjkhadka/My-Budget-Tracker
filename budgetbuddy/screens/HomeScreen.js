fddf0b8208faad8986d28eb5664894749c971a7e
samirjkhadka <findsamirkhadka@gmail.com>
2025-03-04 19:37:29 +0545

    expense schema created

--- budgetbuddy/screens/HomeScreen.js
+++ budgetbuddy/screens/HomeScreen.js
@@ -14,113 +14,120 @@ const HomeScreen = () => {
     setCurrentDate((prevDate) => moment(prevDate).add(1, "month"));
   };
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: "#F8F8F8" }}>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-          marginHorizontal: 12,
-          backgroundColor: "white",
-        }}
-      >
-        <Ionicons name="search-outline" size={23} color="black" />
-        <Text>Money Manager</Text>
-        <Ionicons name="filter-outline" size={23} color="black" />
-      </View>
-      <View
-        style={{
-          flexDirection: "row",
-          alignItems: "center",
-          paddingTop: 15,
-          marginHorizontal: 10,
-          justifyContent: "space-between",
-          backgroundColor: "white",
-        }}
-      >
-        <MaterialIcons
-          onPress={handlePrevMonth}
-          name="chevron-left"
-          size={24}
-          color="black"
-        />
-        <Text>{currentDate.format("MMMM YYYY")}</Text>
-        <MaterialIcons
-          onPress={handleNextMonth}
-          name="chevron-right"
-          size={24}
-          color="black"
-        />
-      </View>
+    <>
+      <SafeAreaView style={{ flex: 1, backgroundColor: "#F8F8F8" }}>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            marginHorizontal: 12,
+            backgroundColor: "white",
+          }}
+        >
+          <Ionicons name="search-outline" size={23} color="black" />
+          <Text>Money Manager</Text>
+          <Ionicons name="filter-outline" size={23} color="black" />
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            paddingTop: 15,
+            marginHorizontal: 10,
+            justifyContent: "space-between",
+            backgroundColor: "white",
+          }}
+        >
+          <MaterialIcons
+            onPress={handlePrevMonth}
+            name="chevron-left"
+            size={24}
+            color="black"
+          />
+          <Text>{currentDate.format("MMMM YYYY")}</Text>
+          <MaterialIcons
+            onPress={handleNextMonth}
+            name="chevron-right"
+            size={24}
+            color="black"
+          />
+        </View>
 
-      <View
-        style={{
-          paddingTop: 15,
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-          gap: 12,
-          marginHorizontal: 12,
-          backgroundColor: "white",
-        }}
-      >
-        <Pressable onPress={() => setOption("Daily")}>
-          <Text
-            style={{
-              color: option === "Daily" ? "black" : "gray",
-              fontSize: 14,
-              fontWeight: "500",
-            }}
-          >
-            Daily
-          </Text>
-        </Pressable>
-        <Pressable onPress={() => setOption("Calendar")}>
-          <Text
-            style={{
-              color: option === "Calendar" ? "black" : "gray",
-              fontSize: 14,
-              fontWeight: "500",
-            }}
-          >
-            Calendar
-          </Text>
-        </Pressable>
-        <Pressable onPress={() => setOption("Monthly")}>
-          <Text
-            style={{
-              color: option === "Monthly" ? "black" : "gray",
-              fontSize: 14,
-              fontWeight: "500",
-            }}
-          >
-            Monthly
-          </Text>
-        </Pressable>
-        <Pressable onPress={() => setOption("Summary")}>
-          <Text
-            style={{
-              color: option === "Summary" ? "black" : "gray",
-              fontSize: 14,
-              fontWeight: "500",
-            }}
-          >
-            Summary
-          </Text>
-        </Pressable>
-        <Pressable onPress={() => setOption("Description")}>
-          <Text
-            style={{
-              color: option === "Description" ? "black" : "gray",
-              fontSize: 14,
-              fontWeight: "500",
-            }}
-          >
-            Description
-          </Text>
+        <View
+          style={{
+            paddingTop: 15,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            gap: 12,
+            marginHorizontal: 12,
+            backgroundColor: "white",
+          }}
+        >
+          <Pressable onPress={() => setOption("Daily")}>
+            <Text
+              style={{
+                color: option === "Daily" ? "black" : "gray",
+                fontSize: 14,
+                fontWeight: "500",
+              }}
+            >
+              Daily
+            </Text>
+          </Pressable>
+          <Pressable onPress={() => setOption("Calendar")}>
+            <Text
+              style={{
+                color: option === "Calendar" ? "black" : "gray",
+                fontSize: 14,
+                fontWeight: "500",
+              }}
+            >
+              Calendar
+            </Text>
+          </Pressable>
+          <Pressable onPress={() => setOption("Monthly")}>
+            <Text
+              style={{
+                color: option === "Monthly" ? "black" : "gray",
+                fontSize: 14,
+                fontWeight: "500",
+              }}
+            >
+              Monthly
+            </Text>
+          </Pressable>
+          <Pressable onPress={() => setOption("Summary")}>
+            <Text
+              style={{
+                color: option === "Summary" ? "black" : "gray",
+                fontSize: 14,
+                fontWeight: "500",
+              }}
+            >
+              Summary
+            </Text>
+          </Pressable>
+          <Pressable onPress={() => setOption("Description")}>
+            <Text
+              style={{
+                color: option === "Description" ? "black" : "gray",
+                fontSize: 14,
+                fontWeight: "500",
+              }}
+            >
+              Description
+            </Text>
+          </Pressable>
+        </View>
+      </SafeAreaView>
+      <View>
+        <Pressable>
+          <Ionicons name="add-circle-outline" size={24} color="black" />
         </Pressable>
       </View>
-    </SafeAreaView>
+    </>
   );
 };
 
